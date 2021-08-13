703708fcb9b8e7bada9f3dd248a1c386debb24ae
franc90 <azarnowski1@gmail.com>
2021-08-13 19:55:59 +0200

    make editor configurable with BRAG_EDITOR env variable

--- src/common.py
+++ src/common.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-EDITOR = 'subl'
+EDITOR = os.getenv('BRAG_EDITOR', 'subl')
 DATA_HOME = Path(os.getenv('XDG_DATA_HOME', Path.home()))
 BRAG_DATA_DIR = DATA_HOME / 'brag' / 'userdata'
 
