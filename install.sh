1cd8382c52bfac12b1d0a40dad2069c46f8c6be0
franc90 <azarnowski1@gmail.com>
2021-08-13 19:40:00 +0200

    update installation process to work everywhere

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-[ ! -d venv ] && python3 -m venv ./venv
-source ./venv/bin/activate
-pip3 install -r requirements.txt
-ln -s $PWD/brag ~/.local/bin/brag
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+[ ! -d venv ] && python3 -m venv "$SCRIPT_DIR/venv"
+source "$SCRIPT_DIR/venv/bin/activate"
+pip3 install -r "$SCRIPT_DIR/requirements.txt"
+ln -s "$SCRIPT_DIR/brag" ~/.local/bin/brag
