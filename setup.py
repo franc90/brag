5a48b1da8dadfa33634d6b3e329c46a20167fac8
franc90 <azarnowski1@gmail.com>
2021-08-15 12:59:13 +0200

    move source files to src dir

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='brag',
     version='0.1',
-    packages=find_packages(),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     install_requires=[
         'pyfzf'
     ],
