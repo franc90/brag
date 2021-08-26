77f4f59e68b931c24aed3d0f6378b018bb0a23de
franc90 <azarnowski1@gmail.com>
2021-08-26 19:19:44 +0200

    add explicite wheel install

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pw
 
 [ ! -d "$SCRIPT_DIR/venv" ] && python3 -m venv "$SCRIPT_DIR/venv"
 source "$SCRIPT_DIR/venv/bin/activate"
+pip3 install wheel
 pip3 install "$SCRIPT_DIR"
 [ ! -L ~/.local/bin/brag ] && ln -s "$SCRIPT_DIR/bin/brag" ~/.local/bin/brag
